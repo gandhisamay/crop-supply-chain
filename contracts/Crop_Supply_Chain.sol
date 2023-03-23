4a8e655c30f5b4c4743c150f7134e72441eccb25
gandhisamay <gandhisamay200@gmail.com>
2023-03-23 15:33:06 +0530

    init

---
+++ contracts/Crop_Supply_Chain.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.14;
+
+import "./Entity_Portal.sol";
+
+contract Crop_Supply_Chain{
+
+    //now here all the magic will happen
+    //that's another doubt.
+    address seed_producer_portal;
+    address farmer_portal;
+    
+    constructor(address s1){
+        seed_producer_portal = 
+    }
+}
